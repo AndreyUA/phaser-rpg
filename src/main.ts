22a683ea4c78975a6e1510e21d5770d76fc40723
AndreyUA <infernalsteel666@gmail.com>
2024-08-30 21:54:21 +0300

    configured game instance

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import "./style.css";
 
-console.log("hello!");
+import Phaser from "phaser";
+
+new Phaser.Game({
+  width: 800,
+  height: 600,
+  title: "RPG Game",
+  url: import.meta.url || "",
+  version: import.meta.env.VERSION || "0.0.1",
+  backgroundColor: "#000000",
+  scale: {
+    // Phaser.Scale.FIT --> stretch the game to fit the screen
+    mode: Phaser.Scale.FIT,
+    // Phaser.Scale.CENTER_BOTH --> center the game on the screen
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+});
