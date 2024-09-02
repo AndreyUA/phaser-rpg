c5e43d813180079cb2518e5dadb9f15d63d22d07
AndreyUA <infernalsteel666@gmail.com>
2024-09-02 13:05:59 +0300

    implemented moving of a player

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -1,8 +1,38 @@
+import { Durotar } from "../scenes/durotar";
 import { SPRITES } from "../utils/constants";
 import { Entity } from "./entity";
 
 export class Player extends Entity {
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+  keys: Phaser.Types.Input.Keyboard.CursorKeys | null = null;
+
+  constructor(scene: Durotar, x: number, y: number, texture: string) {
     super(scene, x, y, texture, SPRITES.PLAYER);
+
+    if (!this.scene.input.keyboard) {
+      return;
+    }
+    this.keys = this.scene.input.keyboard.createCursorKeys();
+  }
+
+  update(): void {
+    if (!this.keys) {
+      return;
+    }
+
+    if (this.keys.left.isDown) {
+      this.x -= 1;
+    }
+
+    if (this.keys.right.isDown) {
+      this.x += 1;
+    }
+
+    if (this.keys.up.isDown) {
+      this.y -= 1;
+    }
+
+    if (this.keys.down.isDown) {
+      this.y += 1;
+    }
   }
 }
--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -36,5 +36,9 @@ export class Durotar extends Phaser.Scene {
     this.player = new Player(this, 400, 250, SPRITES.PLAYER);
   }
 
-  update(time: number, delta: number): void {}
+  update(time: number, delta: number): void {
+    // ! time param --> current time in milliseconds from the start of the game
+    // ! delta param --> time in milliseconds since the last frame
+    this.player?.update();
+  }
 }
